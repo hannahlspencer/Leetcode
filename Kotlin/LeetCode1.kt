dcd3d376955edfd21134a3f884586a9cfef77482
hannahlspencer <hannahlspencer@gmail.com>
2022-06-22 21:17:07 +0100

    Add LeetCode 1

---
+++ Kotlin/LeetCode1.kt
@@ -0,0 +1,16 @@
+class LeetCode1 {
+    // 1. Two Sum
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        var sumMap = HashMap<Int, Int>()
+        for (i in 0..nums.size) {
+            var remaining = target - nums[i]
+            if (sumMap.contains(remaining)) {
+                var first: Int = sumMap.get(remaining) ?: 0
+                var second = i
+                return intArrayOf(first, second)
+            }
+            sumMap.put(nums[i], i)
+        }
+        return intArrayOf()
+    }
+}
