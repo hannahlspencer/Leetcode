ef31284dd047f8c11ae64a4881ff95384fe60b81
hannahlspencer <hannahlspencer@gmail.com>
2022-06-26 11:24:05 +0100

    Add Leetcode21

---
+++ Kotlin/Leetcode21.kt
@@ -0,0 +1,34 @@
+class Leetcode21 {
+    fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+        // 21. Merge Two Sorted Lists
+        var firstNode: ListNode? = ListNode()
+
+        var first = list1
+        var second = list2
+
+        var current = firstNode
+
+        while(first != null && second != null) {
+
+            if(second!!.`val` < first!!.`val`) {
+                current!!.next = second
+                second = second!!.next
+            } else {
+                current!!.next = first
+                first = first!!.next
+            }
+            current = current!!.next
+
+
+        }
+        if(first != null) {
+            current!!.next = first
+        }
+
+        if(second != null) {
+            current!!.next = second;
+        }
+
+        return firstNode!!.next
+    }
+}
